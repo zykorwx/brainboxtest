598c13ee2b27cd3dbb0e234b47390542470d59cd
Saul Enrique Pineda Torres <saul.pineda@gtatelecom.com.mx>
2022-10-22 14:45:50 -0500

    improve our formula section

--- src/components/sections/OurFormulaSection.vue
+++ src/components/sections/OurFormulaSection.vue
@@ -1,14 +1,24 @@
 <template>
-  <div class="flex flex-col justify-center h-full background">
+  <div class="flex flex-col h-full background">
     <div class="w-4/5 p-4 rounde">
       <div class="text-6xl font-bold tracking-wider text-black myShadow">Building the digital future.</div>
     </div>
-    <div class="w-4/5 mt-12 text-2xl tracking-wider">
-      We love to help companies in their digital transformation process using the most efficient technologies and
-      methodologies.
-      These companies, which are markedly start-up in nature, are behind the emergence of new solutions and technical
-      architectures which have shaken up the IT world thanks to tools and ways of building and conceptualizing digital
-      products that did not exist up until very recently.
+    <div class="w-full mt-12 ">
+      <div  class="grid grid-cols-4 p-4 text-lg rounded">
+        <img class="w-full col-span-2 p-4" src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=50" />
+        <div class="self-center col-span-2 ml-4">
+          <span class="text-4xl font-semibold">We love </span> to help companies in their digital transformation process using the most efficient <span class="text-4xl font-semibold">technologies</span>
+          <span class="text-4xl font-semibold"> and methodologies</span>.
+        </div>
+      </div>
+      <div class="grid grid-cols-4 p-4 mt-5 text-lg rounded">
+        <div class="self-center col-span-2 ml-4">
+          <span class="text-4xl font-semibold">We want</span> to put all our talent at the service of a single purpose: making digital products that help to <span class="text-4xl font-semibold"> make the world a
+          better place.</span>
+        </div>
+        <img class="w-full col-span-2 p-4"
+          src="https://images.unsplash.com/photo-1529333166437-7750a6dd5a70?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=50" />
+      </div>
 
     </div>
     <div>
